9f8c8d0d2542c360d50f0ef4412e6ef85edaf886
WhyOnlyTheBest <104170338+WhyOnlyTheBest@users.noreply.github.com>
2022-05-11 15:48:15 -0500

    Title screen
    - Added title screen

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+
+}
